87b88552f79866bd25cca6cc349dfaa3e3b00ffa
naivor <naivortang@gmail.com>
2017-07-16 22:12:41 +0800

    添加LeakCanary

--- sample/src/main/java/com/naivor/sample/NaivorApplication.java
+++ sample/src/main/java/com/naivor/sample/NaivorApplication.java
@@ -19,8 +19,8 @@ package com.naivor.sample;
 import android.app.Application;
 import android.util.Log;
 
-
 import com.naivor.sample.utils.ToastUtil;
+import com.squareup.leakcanary.LeakCanary;
 
 import timber.log.Timber;
 
@@ -33,6 +33,14 @@ public class NaivorApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+
+        LeakCanary.install(this);
 
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
